f8e71f27a6f8d5649021ade24cbbf9696279a7e6
Victor Afanassieff <victor@afanassieff.com>
2020-03-05 14:37:55 +0100

    Fix

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ NAME="bitwarden_rs_dropbox"
 
 cd ./docker
 
-if [ ! -z "$registry" ]; then
+if [ ! -z "registry" ]; then
     # For docker hub ...
     IMAGE_NAME="vafanassieff/$NAME"
     docker build -t $IMAGE_NAME:latest .
-    docker push $IMAGE_NAME:$VERSION
+    docker push $IMAGE_NAME:latest
 
     # For my personal registry ...
     IMAGE_NAME="registry.afa.ovh/$NAME"
     docker build -t $IMAGE_NAME:latest .
-    docker push $IMAGE_NAME:$VERSION
+    docker push $IMAGE_NAME:latest
 else
     IMAGE_NAME="$NAME"
     docker build -t $IMAGE_NAME:dev .
